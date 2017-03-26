da2d5c873f8eadb985a28306708aef50bba8b6a9
X. HU(Mac) <i@finalobject.cn>
2017-03-26 11:05:10 +0800

    add new

---
+++ generate.m
@@ -0,0 +1,13 @@
+% @author finalObject
+%         http://www.finalobject.cn
+%         i@finalobject.cn
+%         https://github.com/finalObject
+% @date 2017Äê3ÔÂ26ÈÕ 11:04:45
+% @version 1.0
+function [ output_args ] = generate( input_args )
+%GENERATE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
