defbf79faa96ca30911891f77a98ce027f5ef8b1
X. HU(Mac) <i@finalobject.cn>
2017-03-26 11:18:05 +0800

    peaks

--- generate.m
+++ generate.m
@@ -4,10 +4,11 @@
 %         https://github.com/finalObject
 % @date 2017Äê3ÔÂ26ÈÕ 11:04:45
 % @version 1.0
-function [ output_args ] = generate( input_args )
+
 %GENERATE Summary of this function goes here
 %   Detailed explanation goes here
 
 
-end
+image = peaks;
 
+mesh(image);
